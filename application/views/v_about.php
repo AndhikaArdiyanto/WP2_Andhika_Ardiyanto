02c270a0a35a6e1f466158e523a37ee012a0c8ab
AndhikaArdiyanto <andhikardnt@gmail.com>
2023-07-03 17:01:17 +0700

    WP2_Andhika_Ardiyanto

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>RIJAL</h4>
+    <ul type="disc">
+        <li>Nama Depan : Andhika</li>
+        <li>Nama Belakang : Ardiyanto</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl.Malioboro</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+        <li>Basket</li>
+    </ul>
+</section>
